785a4c081b9fc05109e706dfafad3ab36718a035
Taylor Robertson <josephtaylorrobertson@gmail.com>
2020-06-22 10:10:51 -0400

    Updates

---
+++ precache-manifest.3b9786faa04b0ad7ea223b8fb17ff2b3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a3ded28bda606918aa6f456971328b95",
+    "url": "/fake-react/index.html"
+  },
+  {
+    "revision": "50e08f9a1e516161b4e9",
+    "url": "/fake-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7478190d3a9a0ed950bf",
+    "url": "/fake-react/static/js/2.decd6d90.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/fake-react/static/js/2.decd6d90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "50e08f9a1e516161b4e9",
+    "url": "/fake-react/static/js/main.39300725.chunk.js"
+  },
+  {
+    "revision": "ba4cda2654a4295e0bfc",
+    "url": "/fake-react/static/js/runtime-main.68fe5917.js"
+  }
+]);
